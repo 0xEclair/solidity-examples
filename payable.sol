42916bf3ec55cbf78f028cc96ee9e07e13675046
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-06-20 08:00:00 +0800

    :sparkles: feat: new `payable` example

--- payable.sol
+++ payable.sol
@@ -3,24 +3,30 @@ pragma solidity ^0.8.13;
 
 contract Payable {
     address payable public owner;
-
+    mapping(address => uint256) public deposits;
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-    function deposit() public payable {}
+    function deposit() public payable {
+        deposits[msg.sender] += msg.value;
+    }
 
-    function notPayable() public {}
+    function notPayable() public {
+
+    }
 
     function withdraw() public {
-        uint amount = address(this).balance;
-
-        (bool success, ) = owner.call{value: amount}("");
-        require(success, "Failed to send Ether");
+        uint256 amount = deposits[msg.sender];
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to send ether");
+        deposits[msg.sender] = 0;
     }
 
     function transfer(address payable to, uint amount) public {
+        require(deposits[msg.sender] >= amount, "invalid amount");
+        deposits[msg.sender] -=amount;
         (bool success, ) = to.call{value: amount}("");
-        require(success, "Failed to send Ether");
+        require(success, "Failed to send ether");
     }
 }
